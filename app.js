fed18f3d334c76e289a6ae2ce6f31ec14e28f0ab
IrumiDesu7 <ikalam89@gmail.com>
2022-04-05 07:23:58 +0800

    added 16x16 grids logic to be added in html

---
+++ app.js
@@ -0,0 +1,15 @@
+const containerDiv = document.createElement('div');
+containerDiv.classList.add('container');
+document.body.appendChild(containerDiv);
+
+for (let i = 1; i <= 16; i++) {
+  const gridDiv = document.createElement('div');
+  gridDiv.classList.add('row-container');
+  containerDiv.appendChild(gridDiv);
+  for (let j = 1; j <= 16; j++) {
+    const grid = document.createElement('div');
+    grid.classList.add('grid');
+    grid.textContent = `GRID-${j}`;
+    gridDiv.appendChild(grid);
+  }
+}
