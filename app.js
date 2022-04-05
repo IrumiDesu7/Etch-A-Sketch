859ebd8053de8346e1cb079b047e05d06a477216
IrumiDesu7 <ikalam89@gmail.com>
2022-04-05 08:35:41 +0800

    add button to the colored grid(s)

--- app.js
+++ app.js
@@ -1,15 +1,23 @@
+const buttonContainer = document.createElement('div');
+buttonContainer.classList.add('container');
+document.body.appendChild(buttonContainer);
+const clearBtn = document.createElement('button');
+clearBtn.classList.add('clear-button');
+clearBtn.textContent = 'Clear';
+buttonContainer.appendChild(clearBtn);
+let gridDiv, grid;
+
 const containerDiv = document.createElement('div');
 containerDiv.classList.add('container');
 document.body.appendChild(containerDiv);
 
 for (let i = 1; i <= 16; i++) {
-  const gridDiv = document.createElement('div');
+  gridDiv = document.createElement('div');
   gridDiv.classList.add('row-container');
   containerDiv.appendChild(gridDiv);
   for (let j = 1; j <= 16; j++) {
-    const grid = document.createElement('div');
+    grid = document.createElement('div');
     grid.classList.add('grid');
-    grid.textContent = `GRID-${j}`;
     gridDiv.appendChild(grid);
   }
 }
@@ -20,3 +28,7 @@ gridItems.forEach((e) =>
     e.classList.add('color');
   })
 );
+
+clearBtn.addEventListener('click', function () {
+  gridItems.forEach((e) => e.classList.remove('color'));
+});
